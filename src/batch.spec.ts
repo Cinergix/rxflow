e06b780002c05b34554779fcc86caf042b236bd1
Muhammed Thanish <mnmtanish@gmail.com>
2017-07-21 14:31:34 +0530

    Refactor batch function tests

--- src/batch.spec.ts
+++ src/batch.spec.ts
@@ -1,78 +1,86 @@
 import { Observable } from 'rxjs';
 import { batch } from './batch';
 
-describe('rxflow', () => {
-  describe('batch', () => {
-    const TEST_BATCH_TIMEOUT = 50;
+describe('batch', () => {
+  const TEST_BATCH_TIMEOUT = 50;
 
-    function batchAndGetResult(input: Observable<any>) {
-      return batch(TEST_BATCH_TIMEOUT, input).toArray().toPromise();
-    }
-
-    it('should return an observables', () => {
-      const out = batch(TEST_BATCH_TIMEOUT, Observable.of(null));
-      expect(out instanceof Observable).toBe(true);
+  function createTestObservables(...sizes: number[]): [number[][], Observable<number>] {
+    const values = sizes.map(size => {
+      const group = [];
+      for (let i = 0; i < size; i++) {
+        group.push(Math.random());
+      }
+      return group;
     });
+    const observable = values
+      .map((group, idx) => Observable.from(group).delay(idx * TEST_BATCH_TIMEOUT + 10))
+      .reduce((merged, obs) => merged.concat(obs));
+    return [values, observable];
+  }
 
-    describe('single event', () => {
-      it('should emit events in S1,E1,N1 order', async () => {
-        const val = Math.random();
-        const out = await batchAndGetResult(Observable.of(val));
-        expect(out).toEqual([
-          { type: 'start', event: val },
-          { type: 'event', event: val },
-          { type: 'end', event: val },
-        ]);
-      });
+  function batchAndGetResult(input: Observable<any>) {
+    return batch(TEST_BATCH_TIMEOUT, input).toArray().toPromise();
+  }
+
+  it('should return an observables', () => {
+    const out = batch(TEST_BATCH_TIMEOUT, Observable.of(null));
+    expect(out instanceof Observable).toBe(true);
+  });
+
+  describe('single event', () => {
+    it('should emit events in S1,E1,N1 order', async () => {
+      const [values, observable] = createTestObservables(1);
+      const out = await batchAndGetResult(observable);
+      expect(out).toEqual([
+        { type: 'start', event: values[0][0] },
+        { type: 'event', event: values[0][0] },
+        { type: 'end', event: values[0][0] },
+      ]);
     });
+  });
 
-    describe('single 2 event batch (sync)', () => {
-      it('should emit events in S1,E1,E2,N2 order', async () => {
-        const val = [Math.random(), Math.random()];
-        const out = await batchAndGetResult(Observable.of(...val));
-        expect(out).toEqual([
-          { type: 'start', event: val[0] },
-          { type: 'event', event: val[0] },
-          { type: 'event', event: val[1] },
-          { type: 'end', event: val[1] },
-        ]);
-      });
+  describe('single 2 event batch (sync)', () => {
+    it('should emit events in S1,E1,E2,N2 order', async () => {
+      const [values, observable] = createTestObservables(2);
+      const out = await batchAndGetResult(observable);
+      expect(out).toEqual([
+        { type: 'start', event: values[0][0] },
+        { type: 'event', event: values[0][0] },
+        { type: 'event', event: values[0][1] },
+        { type: 'end', event: values[0][1] },
+      ]);
     });
+  });
 
-    describe('2 single event batches (sync)', () => {
-      it('should emit events in S1,E1,N1,S2,E2,N2 order', async () => {
-        const val = [Math.random(), Math.random()];
-        const ob1 = Observable.of(val[0]);
-        const ob2 = Observable.of(val[1]).delay(TEST_BATCH_TIMEOUT + 10);
-        const out = await batchAndGetResult(ob1.concat(ob2));
-        expect(out).toEqual([
-          { type: 'start', event: val[0] },
-          { type: 'event', event: val[0] },
-          { type: 'end', event: val[0] },
-          { type: 'start', event: val[1] },
-          { type: 'event', event: val[1] },
-          { type: 'end', event: val[1] },
-        ]);
-      });
+  describe('2 single event batches (sync)', () => {
+    it('should emit events in S1,E1,N1,S2,E2,N2 order', async () => {
+      const [values, observable] = createTestObservables(1, 1);
+      const out = await batchAndGetResult(observable);
+      expect(out).toEqual([
+        { type: 'start', event: values[0][0] },
+        { type: 'event', event: values[0][0] },
+        { type: 'end', event: values[0][0] },
+        { type: 'start', event: values[1][0] },
+        { type: 'event', event: values[1][0] },
+        { type: 'end', event: values[1][0] },
+      ]);
     });
+  });
 
-    describe('multiple 2 event batches (sync)', () => {
-      it('should emit events in S1,E1,E2,N2,S3,E3,E4,N4 order', async () => {
-        const val = [Math.random(), Math.random(), Math.random(), Math.random()];
-        const ob1 = Observable.from(val.slice(0, 2));
-        const ob2 = Observable.from(val.slice(2)).delay(TEST_BATCH_TIMEOUT + 10);
-        const out = await batchAndGetResult(ob1.concat(ob2));
-        expect(out).toEqual([
-          { type: 'start', event: val[0] },
-          { type: 'event', event: val[0] },
-          { type: 'event', event: val[1] },
-          { type: 'end', event: val[1] },
-          { type: 'start', event: val[2] },
-          { type: 'event', event: val[2] },
-          { type: 'event', event: val[3] },
-          { type: 'end', event: val[3] },
-        ]);
-      });
+  describe('multiple 2 event batches (sync)', () => {
+    it('should emit events in S1,E1,E2,N2,S3,E3,E4,N4 order', async () => {
+      const [values, observable] = createTestObservables(2, 2);
+      const out = await batchAndGetResult(observable);
+      expect(out).toEqual([
+        { type: 'start', event: values[0][0] },
+        { type: 'event', event: values[0][0] },
+        { type: 'event', event: values[0][1] },
+        { type: 'end', event: values[0][1] },
+        { type: 'start', event: values[1][0] },
+        { type: 'event', event: values[1][0] },
+        { type: 'event', event: values[1][1] },
+        { type: 'end', event: values[1][1] },
+      ]);
     });
   });
 });
